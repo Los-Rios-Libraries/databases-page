ca1ecc1db2205aa5b4ff71f01bfdce239c5cda41
jkarlsen <karlsej@scc.losrios.edu>
2015-06-23 23:48:22 -0700

    Google Analytics

--- index.php
+++ index.php
@@ -44,17 +44,21 @@ include_once('functions.php');
 <meta charset="utf-8" >
  <meta name=viewport content="width=device-width, initial-scale=1">
 <title><?php echo $metaTitle; ?> Research Databases - Los Rios Libraries</title>
-<!--
-<link rel="stylesheet" href="res/jquery-ui.min.css">
 
- <link rel="stylesheet" href="res/slicknav.min.css" /> 
- <link rel="stylesheet" href="res/sm-core-css.css" />
- <link rel="stylesheet" href="res/jquery.sidr.dark.css" />-->
 <link rel="stylesheet" href="style.css" >
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic' rel='stylesheet' type='text/css'>
  
 
-<!-- <script src="res/jquery-ui.min.js"></script> -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-44798235-8', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </head>
 <body>
  <header>
