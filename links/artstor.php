31bb5e40623fd219aeb3dc8bb641ee75acfdb1c2
jkarlsen <karlsej@scc.losrios.edu>
2015-09-01 15:47:24 -0700

    Initial commit

---
+++ links/artstor.php
@@ -0,0 +1,19 @@
+<?php
+include 'location.php';
+$path = "";
+if ((isset($oncampus)) && (!isset($_GET['offcampus'])) ){
+    $URLbase = "http://library.artstor.org/";
+    header ('Location: ' .$URLbase . $path);
+
+}
+else {
+    $problemDB = 'artstor';
+include 'problems/shell.php';
+//    $URLbase = "http://0-library.artstor.org.lasiii.losrios.edu/";
+//		$message = "offcampus";
+}
+// 
+//echo $message;
+
+?>
+
---
+++ links/problems/dbs/artstor.php
@@ -0,0 +1,15 @@
+<p>
+    
+    Artstor is currently working very slowly, and in some cases not at all, from off-campus. We are working with the vendor to remedy this problem. In the meantime, you can use the following process to access Artstor.
+</p>
+<p><strong>Do you have an Artstor user account?</strong></p>
+<p>If you do, <a  class="db-name" href="http://library.artstor.org/">go to Artstor</a> and sign in.</p>
+<p>If you do not:</p>
+
+<ol>
+    <li>Visit the landing page of Artstor via <a class="db-name" href="http://0-library.artstor.org.lasiii.losrios.edu/library/">this link.</a></li>
+    <li>Register for a free Artstor account.</li>
+    <li>Once you've registered for the account, visit Artstor directly at <a class="db-name" href="http://library.artstor.org/">http://library.artstor.org/</a> rather than going through the Los Rios Research Databases page. Sign in using the account you created.</li>
+</ol>
+<p>The following video walks you through the process of creating your user account:</p>
+<iframe width="560" height="315" src="//www.youtube.com/embed/KCfSNhBdjeA" frameborder="0" allowfullscreen></iframe>
