4cdf6964448f9f3e1ccc7e7ef62cbd952022bd81
jkarlsen <karlsej@scc.losrios.edu>
2015-11-05 10:47:18 -0800

    Adjusted search form so that OneSearch is default, database is option, pub finder no longer there

--- search-form.php
+++ search-form.php
@@ -4,22 +4,20 @@
  <div id="form-remainder" class="hidden">
  <div id="radio-intro">What are you looking for?</div>
  <div id="search-options">
- <div>
-  <input type="radio" name="search-type" value="dbpage" id="search-db" checked >
-  <label for="search-db" >A database</label>
- </div>
- <div class="search-exp hidden">We'll check this site to see if the library provides access to a certain database</div>
- <div> <input type="radio" name="search-type" value="az" id="search-journal">
- <label for="search-journal" >A journal, magazine or other publication</label>
- </div>
- <div class="search-exp hidden">We'll send your keywords to the Publication Finder, which tells you which database contains a particular publication</div>
- <div>
-  <input type="radio" name="search-type" value="onesearch" id="search-onesearch">
-  <label for="search-onesearch" >Articles, ebooks or other items</label>
+   <div>
+  <input type="radio" name="search-type" value="onesearch" id="search-onesearch" checked >
+  <label for="search-onesearch" >Articles, ebooks, journals and other items</label>
  </div>
 
   <div class="search-exp hidden">We'll send your keywords to OneSearch, which helps you find articles, ebooks &amp; more across a large number of databases</div>
    </div>
+ <div>
+  <input type="radio" name="search-type" value="dbpage" id="search-db" >
+  <label for="search-db" >An individual database</label>
+ </div>
+ <div class="search-exp hidden">We'll check this site to see if the library provides access to a certain database</div>
+
+
   <input type="submit" value="search">
      <button type="button" id="form-closer">X Close</button>
 </div>
--- search.php
+++ search.php
@@ -3,16 +3,15 @@ $query = $_POST['find'];
 $resource = $_POST['search-type'];
 
 switch ($resource){
+case 'onesearch':
+    $url = 'http://0-search.ebscohost.com.lasiii.losrios.edu/login.aspx?authtype=ip&groupid=main&profile=eds&direct=true&site=eds-live&bquery=' . urlencode($query);
+    break;
+    
 case 'dbpage':
     $url = 'index.php?az&query=' .urlencode($query);
     break;
 
-case 'az':
-    $url = 'http://atoz.ebsco.com/Titles/SearchResults/9823?SearchType=Contains&Find='. urlencode($query) . '&GetResourcesBy=QuickSearch';
-    break;
-case 'onesearch':
-    $url = 'http://0-search.ebscohost.com.lasiii.losrios.edu/login.aspx?authtype=ip&groupid=main&profile=eds&direct=true&site=eds-live&bquery=' . urlencode($query);
-    break;
+
     }
 header('Location: ' .$url);
 ?>
