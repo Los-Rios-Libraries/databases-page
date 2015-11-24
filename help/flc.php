ec22eac861e7df5fad50256339f2032d3a1d967f
jkarlsen <karlsej@scc.losrios.edu>
2015-11-23 16:50:36 -0800

    Zopim change, also images now on databases server, for https

--- help/flc.php
+++ help/flc.php
@@ -10,17 +10,17 @@
 <p><em>We are always happy to help!</em></p>
 <div id="services">
     <div class="service">
-        <a href="http://www.flc.losrios.edu/libraries/ask-a-librarian"><img alt="Online Help Form" src="http://lgimages.s3.amazonaws.com/data/imagemanager/43489/chat01.png" height="29" width="29"></a>
+        <a href="http://www.flc.losrios.edu/libraries/ask-a-librarian"><img alt="Online Help Form" src="help/flc-img/chat01.png" height="29" width="29"></a>
         <h3>Ask A Librarian</h3>
         <p>    <a href="http://www.flc.losrios.edu/libraries/ask-a-librarian">Online Help Form</a></p>
     </div>
     <div class="service">
-        <img alt="Txt" src="http://lgimages.s3.amazonaws.com/data/imagemanager/43489/diagram-34.png">
+        <img alt="Txt" src="help/flc-img/diagram-34.png">
         <h3>Call Us</h3>
         <p>916-608-6612</p>
     </div>
     <div class="service">
-    <img  alt="Visit Us" src="http://lgimages.s3.amazonaws.com/data/imagemanager/43489/user_friend.png" height="32" width="32">
+    <img  alt="Visit Us" src="help/flc-img/user_friend.png" height="32" width="32">
     <h3>Visit Us</h3>
     <p>Reference Desk</p>
     <p>FLC Main Campus Library</p>
@@ -46,6 +46,6 @@
 
     }
     })(); */
-hideZ();
+// hideZ();
 checkCookies('newWindowLinks');
 </script>
