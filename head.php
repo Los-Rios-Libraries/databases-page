00ba1a8ff7c951de745dceb4063860def0a46ce3
jkarlsen <karlsej@scc.losrios.edu>
2016-04-04 09:50:37 -0700

    Initial commit

---
+++ head.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=EDGE">
+<meta charset="utf-8" >
+ <meta name=viewport content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="style.css?0205" >
+<link rel="stylesheet" href="res/jquery-ui.css">
+<link rel="stylesheet" href="survey.css">
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,400italic' rel='stylesheet' type='text/css'>
+ 
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-44798235-7', 'auto');
+  ga('send', 'pageview');
+  ga('set', 'anonymizeIp', true);
+
+</script>
