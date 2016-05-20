2b42e13f317cb5963119530ab73e8a030aee25f3
jkarlsen <karlsej@scc.losrios.edu>
2016-05-20 15:06:02 -0700

    Initial? not currently used

--- links/artstor.php
+++ links/artstor.php
@@ -3,17 +3,18 @@ include 'location.php';
 $path = "";
 if ((isset($oncampus)) && (!isset($_GET['offcampus'])) ){
     $URLbase = "http://library.artstor.org/";
-    header ('Location: ' .$URLbase . $path);
+//    header ('Location: ' .$URLbase . $path);
 
 }
 else {
-    $problemDB = 'artstor';
-include 'problems/shell.php';
-//    $URLbase = "http://0-library.artstor.org.lasiii.losrios.edu/";
+ //   $problemDB = 'artstor';
+// include 'problems/shell.php';
+    $URLbase = "http://0-library.artstor.org.lasiii.losrios.edu/";
 //		$message = "offcampus";
 }
 // 
 //echo $message;
+header ('Location: ' .$URLbase . $path);
 
 ?>
 
