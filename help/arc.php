e3736e46398ce5f38696364e83f88b7bd3b6c5ca
Kate Williamson <w1576672@apps.losrios.edu>
2016-07-21 00:36:12 +0000

    changed chat code snippet number

--- help/arc.php
+++ help/arc.php
@@ -18,7 +18,7 @@
 <script type="text/javascript">
   (function() {
     var x = document.createElement("script"); x.type = "text/javascript"; x.async = true;
-    x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "libraryh3lp.com/js/libraryh3lp.js?8699";
+    x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "libraryh3lp.com/js/libraryh3lp.js?9603";
     var y = document.getElementsByTagName("script")[0]; y.parentNode.insertBefore(x, y);
   })();
 </script>
