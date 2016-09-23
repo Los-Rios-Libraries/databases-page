167edc0eb6028a6e8c5d244e5ab78c6f237554f5
jkarlsen <karlsej@scc.losrios.edu>
2016-09-23 12:47:35 -0700

    Alter analytics.js call to allow for opted-out users

--- head.php
+++ head.php
@@ -12,12 +12,16 @@
  
 
 <script>
+  var gaID = 'UA-44798235-7';
+  if (document.cookie.indexOf('lrGAOptOut=y') > -1) {
+    window['ga-disable-' + gaID + ''] = true;
+  }
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-44798235-7', 'auto');
+  ga('create', '' + gaID + '', 'auto');
   ga('send', 'pageview');
   ga('set', 'anonymizeIp', true);
 
