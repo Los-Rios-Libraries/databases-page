9f31f78eca93e9d880139891efdd8b6f1811190c
jkarlsen <karlsej@scc.losrios.edu>
2017-01-31 08:22:28 -0800

    Initial commit

---
+++ footer.php
@@ -0,0 +1,27 @@
+<footer id="lr-footer">
+    <div id="ft-content-container">
+        <div id="col-container">
+        <div id="ft-head">Los Rios Libraries</div>
+        <div id="col1" class="foot-col">
+            <div><a href="http://www.arc.losrios.edu/arclibrary.htm">American River College Library</a></div> 
+           <!--  <div><a href="http://www.arc.losrios.edu/arclibrary.htm"><abbr title="American River College">ARC</abbr> Library</a></div>-->
+            <div>916-484-8455</div>
+        </div>
+        <div id="col2" class="foot-col">
+            <div><a href="http://www.crc.losrios.edu/library">Cosumnes River College Library</a></div> 
+            <!-- <div><a href="http://www.crc.losrios.edu/library"><abbr title="Cosumnes River College">CRC</abbr> Library</a></div>-->
+            <div>916-691-7266</div>
+        </div>
+        <div id="col3" class="foot-col">
+            <div><a href="http://www.flc.losrios.edu/libraries">Folsom Lake College Library</a></div> 
+            <!-- <div><a href="http://www.flc.losrios.edu/libraries"><abbr title="Folsom Lake College">FLC</abbr> Library</a></div>-->
+            <div>916-608-6613</div>
+        </div>
+        <div id="col4" class="foot-col">
+            <div><a href="http://www.scc.losrios.edu/library">Sacramento City College Library</a></div> 
+            <!-- <div><a href="http://www.scc.losrios.edu/library"><abbr title="Sacramento City College">SCC</abbr> Library</a></div>-->
+            <div>916-558-2301</div>
+        </div>
+        </div>
+    </div>
+</footer>
