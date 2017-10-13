88252d91078b9dfb0aae7b6583410c8cca34863d
jkarlsen <karlsej@scc.losrios.edu>
2017-10-13 11:03:05 -0700

    Initial commit

---
+++ special/artstor.php
@@ -0,0 +1,16 @@
+<p>
+   Artstor is currently experiencing problems when accessed from off-campus.</p>
+<p>Until this problem is resolved, please take the following steps:
+</p>
+<ol>
+    <li>
+    Go to Artstor at <a href="http://0-library.artstor.org.lasiii.losrios.edu">http://0-library.artstor.org.lasiii.losrios.edu</a> 
+        
+    </li>
+     <li>
+        Click the <strong>Register</strong> link to create a free account.
+     </li>
+     <li>Once you&apos;ve registered, go directly to <a href="http://library.artstor.org">http://library.artstor.org</a> and log in to use Artstor.</li>
+</ol>
+
+
