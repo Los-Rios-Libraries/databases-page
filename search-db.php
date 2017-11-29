4ce2286d694448608bc1f94b3187dee57c964200
jkarlsen <karlsej@scc.losrios.edu>
2017-11-29 11:20:04 -0800

    Replace lengthy conditionals with key/value arrays + loops

--- search-db.php
+++ search-db.php
@@ -5,260 +5,199 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
 */
+function findURL($arr, $query) {
+    if ($query !== '') {
+        return $arr['queryUrl'];
+    }
+    else {
+        return $arr['noquery'];
+    }  
+}
+
 $dbName = $_POST['db-name'];
  $query = $_POST['query'];
  $query = urlencode($query);
 $vendor = $_POST['vendor'];
 $ehostCode = $_POST['ehost'];
 $url = '';
-$ebscoBase = 'http://0-search.ebscohost.com.lasiii.losrios.edu/login.aspx?authtype=ip';
-if ($vendor === 'ebsco') {
-    if (isset($ehostCode)) {
-        if ($query!== ''){
-    $ebscoRemainder = '&direct=true&db=' . $ehostCode . '&bquery=' .$query . '&site=ehost-live&scope=site';
-    $url = $ebscoBase . $ebscoRemainder;
-        }
-        else {
-            $url = $ebscoBase . '&profile=ehost&defaultdb=' . $ehostCode;
+$homeLibrary = '';
+$galeID = '';
+$fodWid = '';
+if (isset($_COOKIE['homeLibrary'])) {
+$homeLibrary = $_COOKIE['homeLibrary'];
+}
+$collegeVars = array(
+  array(
+    'homeLib' => 'arc',
+    'galeID' => 'sacr22807',
+    'fodWid' => '240535'
+    ),
+  array(
+    'homeLib' => 'crc',
+    'galeID' => 'sacr73031',
+    'fodWid' => '237206'
+    ),
+  array(
+    'homeLib' => 'flc',
+    'galeID' => 'sacr88293',
+    'fodWid' => '237742'
+    ),
+  array(
+    'homeLib' => 'scc',
+    'galeID' => 'cclc_sac',
+    'fodWid' => '106093'
+    )
+);
+
+for ($i = 0; $i < count($collegeVars); $i++) {
+    if ($homeLibrary === $collegeVars[$i]['homeLib']) {
+        $galeID = $collegeVars[$i]['galeID'];
+        $fodWid = $collegeVars[$i]['fodWid'];
+        
+    }
+}
+$proxyStr = '.lasiii.losrios.edu/';
+$ebscoBase = 'http://0-search.ebscohost.com' . $proxyStr .'login.aspx?authtype=ip';
+$dbs = array(
+             array( // first array element is ehost patterns - treated differently
+                'vendor' => 'ebsco',
+                'ehostcode' => $ehostCode,
+                'noquery' => $ebscoBase . '&profile=ehost&defaultdb=' . $ehostCode,
+                'queryUrl' => $ebscoBase . '&direct=true&db=' . $ehostCode . '&bquery=' .$query . '&site=ehost-live&scope=site'
+             ),
+             array(
+                'vendor' => 'ebsco',
+                'dbname' => 'onesearch',
+                'noquery' => $ebscoBase . '&profile=eds',
+                'queryUrl' => $ebscoBase . '&direct=true&bquery=' . $query . '&profile=eds&site=eds-live&scope=site'
+             ),
+             array(
+                'vendor' => 'ebsco',
+                'dbname' => 'ebook',
+                'noquery' => $ebscoBase . '&profile=eds&defaultdb=nlebk',
+                'queryUrl' => $ebscoBase . '&direct=true&db=nlebk&bquery=' . $query . '&profile=ebooks&site=ehost-live&scope=site'
+             ),
+            array(
+                'vendor' => 'ebsco',
+                'dbname' => 'small',
+                'noquery' => $ebscoBase . '&profile=sbrc',
+                'queryUrl' =>'&direct=true&profile=sbrc&bquery=' . $query .'&site=sbrc-live&scope=site'
+             ),
+            array(
+                'vendor' => 'ebsco',
+                'dbname' => 'business source',
+                'noquery' => $ebscoBase . '&profile=bsc',
+                'queryUrl' => $ebscoBase . '&direct=true&bquery=' . $query .'&profile=bsc&site=bsc-live&scope=site'
+             ),
             
-        }
-    }
-    elseif (strpos($dbName, 'onesearch') > -1) {
-        if ($query !== '') {
-        $url = $ebscoBase . '&direct=true&bquery=' . $query . '&profile=eds&site=eds-live&scope=site';
-        }
-        else {
-            $url = $ebscoBase . '&profile=eds';
-        }
-    }
-    elseif (strpos($dbName,'ebook') > -1) {
-        if ($query !== '') {
-        $url = $ebscoBase . '&direct=true&db=nlebk&bquery=' . $query . '&profile=ebooks&site=ehost-live&scope=site';
-        }
-        else {
-            $url = $ebscoBase . '&profile=eds&defaultdb=nlebk';
-        }
-    }
-        elseif (strpos($dbName, 'small') > -1) {
-        if ($query !== '') {
-        $url = $ebscoBase . '&direct=true&profile=sbrc&bquery=' . $query .'&site=sbrc-live&scope=site';
-        }
-        else {
-        $url = $ebscoBase . '&profile=sbrc';
-        }
-    }
-    elseif (strpos($dbName, 'business') > -1) {
-        if ($query !== '') {
-        $url = $ebscoBase . '&direct=true&bquery=' . $query .'&profile=bsc&site=bsc-live&scope=site';
-        }
-        else {
-        $url = $ebscoBase . '&profile=bsc';
-        }
-    }
-    elseif (strpos($dbName, 'literary') > -1) {
-        if ($query !== '') {
-        $url = $ebscoBase . '&direct=true&db=lkh&bquery=' . $query .'&site=lrc-plus&scope=site';
-        }
-        else {
-            $url = $ebscoBase . '&profile=plus';
-        }
-    }
-       elseif (strpos($dbName, 'rehabilitation') > -1) {
-        if ($query !== '') {
-        $url = $ebscoBase . '&direct=true&db=nrcn&db=rrc&bquery=' . $query .'&site=rrc-live&scope=site';
-        }
-        else {
-            $url = $ebscoBase . '&profile=rrc';
-        }
-    }
-    elseif(strpos($dbName, 'consumer') > -1) {
-        if ($query !== '') {
-        $url = $ebscoBase . '&direct=true&bquery=' . $query .'&site=chc-live&scope=site';
-        }
-        else {
-            $url = $ebscoBase . '&profile=chc';
-        }
-    }
-        elseif(strpos($dbName, 'explora') > -1) {
-        if ($query !== '') {
-        $url = $ebscoBase . '&direct=true&bquery=' . $query .'&site=src_ic-live&scope=site';
-        }
-        else {
-            $url = $ebscoBase . '&profile=src_ic';
-        }
-    }
-}
-elseif (strpos($vendor, 'cq') > -1) {
-    $urlBase = 'http://0-library.cqpress.com.lasiii.losrios.edu/cqresearcher/';
-    if ($query !== '') {
-    $url = $urlBase . 'cqresearcher/search.php?fulltext=' .$query .' &action=newsearch&sort=custom%3Asorthitsrank%2Cd&x=0&y=0';
-    }
-    else {
-        $url = $urlBase;
-    }
-}
-elseif (strpos($vendor, 'google') > -1) {
-    $urlBase = 'http://0-scholar.google.com.lasiii.losrios.edu/';
-    if ($query !== '') {
-        $url = $urlBase . 'scholar?hl=en&q=' .$query;
-    }
-    else {
-        $url = $urlBase;
-    }
-}
-elseif (strpos($vendor, 'jstor') > -1) {
-    $urlBase = 'http://0-www.jstor.org.lasiii.losrios.edu/action/';
-    if ($query !== '') {
-        $url = $urlBase . 'doAdvancedSearch?q0=' .$query;
-        }
-    else {
-        $url = $urlBase . 'showAdvancedSearch';
-    }
-}
-elseif (strpos($dbName, 'ebook-central') > -1) {
-    $urlBase = 'http://0-ebookcentral.proquest.com.lasiii.losrios.edu/lib/losrios/';
-    if ($query !== '') {
-        $url = $urlBase . 'search.action?query=' . $query;
-    }
-    else {
-        $url = $urlBase;
-    }
-    
-}
-elseif (strpos($dbName, 'pubmed') > -1) {
-    $urlBase = 'http://0-www.ncbi.nlm.nih.gov.lasiii.losrios.edu/pubmed';
-    if ($query !== '') {
-    $url = $urlBase . '/?term=' . $query . '&myncbishare=casccllib';
-    }
-    else {
-        $url = $urlBase . '?myncbishare=casccllib';
-    }
-}
-elseif (strpos($dbName, 'gale-virtual') > -1) {
-    $urlBase = 'http://0-go.galegroup.com.lasiii.losrios.edu';
-    if ($query !== '') {
-        $homeLibrary = $_COOKIE['homeLibrary'];
-        switch ($homeLibrary) {
-            case 'arc':
-                $galeID = 'sacr22807';
-                break;
-            case 'scc':
-                $galeID = 'cclc_sac';
-                break;
-            case 'crc':
-                $galeID = 'sacr73031';
-                break;
-            case 'flc':
-                $galeID = 'sacr88293';
-                break;
-            default:
-                $galeID = 'sacr28903';
-            }
-        $url = $urlBase . '/ps/i.do?dblist=GVRL&st=T003&qt=OQE~' .$query . '&sw=w&ty=bs&it=search&p=GVRL&s=RELEVANCE&u=' . $galeID . '&v=2.1';
-    }
-    else {
-        $url = 'http://0-infotrac.galegroup.com.lasiii.losrios.edu/itweb?db=GVRL';
-    }
-}
-elseif (strpos($vendor, 'american') > -1) {
-    $urlBase = 'http://0-quod.lib.umich.edu.lasiii.losrios.edu/cgi/t/text/text-idx?c=acls';
-    if ($query !== '') {
-    $url = $urlBase . '&cc=acls&op2=and&rgn2=series&sort=freq&type=simple&q1=' . $query .'&rgn1=full+text&q2=ACLS+Humanities+E-Book&cite1=&cite1restrict=author&cite2=&cite2restrict=author&Submit=Search';
+            array(
+                'vendor' => 'ebsco',
+                'dbname' => 'literary',
+                'noquery' => $ebscoBase . '&profile=plus',
+                'queryUrl' => $ebscoBase . '&direct=true&db=lkh&bquery=' . $query .'&site=lrc-plus&scope=site'
+             ),
+            array(
+                'vendor' => 'ebsco',
+                'dbname' => 'rehabilitation',
+                'noquery' =>  $ebscoBase . '&profile=rrc',
+                'queryUrl' => $ebscoBase . '&direct=true&db=nrcn&db=rrc&bquery=' . $query .'&site=rrc-live&scope=site'
+             ),
+            array(
+                'vendor' => 'ebsco',
+                'dbname' => 'consumer',
+                'noquery' =>  $ebscoBase . '&profile=chc',
+                'queryUrl' => $ebscoBase . '&direct=true&bquery=' . $query .'&site=chc-live&scope=site'
+             ),
+            array(
+                'vendor' => 'ebsco',
+                'dbname' => 'explora',
+                'noquery' =>  $ebscoBase . '&profile=src_ic',
+                'queryUrl' => $ebscoBase . '&direct=true&bquery=' . $query .'&site=src_ic-live&scope=site'
+             ),
+             array(
+                'vendor' => 'google',
+                'dbname' => 'google',
+                'noquery' =>  'http://0-scholar.google.com'. $proxyStr,
+                'queryUrl' => 'http://0-scholar.google.com'. $proxyStr . 'scholar?hl=en&q=' .$query
+             ),
+             array(
+                'vendor' => 'jstor',
+                'dbname' => 'jstor',
+                'noquery' =>  'http://0-www.jstor.org'. $proxyStr . 'action/showAdvancedSearch',
+                'queryUrl' => 'http://0-www.jstor.org'. $proxyStr . 'action/doAdvancedSearch?q0=' .$query
+             ),
+             array(
+                'vendor' => 'pubmed',
+                'dbname' => 'pubmed',
+                'noquery' =>  'http://0-www.ncbi.nlm.nih.gov'. $proxyStr . 'pubmed?myncbishare=casccllib',
+                'queryUrl' => 'http://0-www.ncbi.nlm.nih.gov'. $proxyStr . 'pubmed?/?term=' . $query . '&myncbishare=casccllib'
+             ),
+             array(
+                'vendor' => 'gale',
+                'dbname' => 'gale-virtual',
+                'noquery' =>  'http://0-infotrac.galegroup.com'. $proxyStr . 'itweb?db=GVRL',
+                'queryUrl' => 'http://0-go.galegroup.com'. $proxyStr . 'ps/i.do?dblist=GVRL&st=T003&qt=OQE~' .$query . '&sw=w&ty=bs&it=search&p=GVRL&s=RELEVANCE&u=' . $galeID . '&v=2.1'
+             ),
+             array(
+                'vendor' => 'american',
+                'dbname' => 'acls-humanities',
+                'noquery' =>  'http://0-quod.lib.umich.edu'. $proxyStr . 'cgi/t/text/text-idx?c=acls;rgn=full%20text;page=simple',
+                'queryUrl' => 'http://0-quod.lib.umich.edu'. $proxyStr . 'cgi/t/text/text-idx?c=acls&cc=acls&op2=and&rgn2=series&sort=freq&type=simple&q1=' . $query .'&rgn1=full+text&q2=ACLS+Humanities+E-Book&cite1=&cite1restrict=author&cite2=&cite2restrict=author&Submit=Search'
+             ),
+             array(
+                'vendor' => 'oxford',
+                'dbname' => 'oxford-art',
+                'noquery' =>  'http://0-www.oxfordartonline.com'. $proxyStr,
+                'queryUrl' => 'http://0-www.oxfordartonline.com'. $proxyStr . 'subscriber/search_results?q=' . $query
+             ),
+             array(
+                'vendor' => 'oxford',
+                'dbname' => 'oxford-english',
+                'noquery' =>  'http://0-www.oed.com'. $proxyStr,
+                'queryUrl' => 'http://0-www.oed.com'. $proxyStr . 'search?searchType=dictionary&q=' . $query
+             ),
+             array(
+                'vendor' => 'infobase',
+                'dbname' => 'films',
+                'noquery' =>  'http://0-digital.films.com'. $proxyStr . 'PortalPlaylists.aspx?wid=' .$fodWid,
+                'queryUrl' => 'http://0-digital.films.com'. $proxyStr . 'PortalPlaylists.aspx?wid=' .$fodWid .'&rd=a&q=' . $query
+             ),
+             array(
+                'vendor' => 'proquest',
+                'dbname' => 'resources',
+                'noquery' =>  'http://0-www.rclweb.net'. $proxyStr,
+                'queryUrl' => 'http://0-www.rclweb.net'. $proxyStr . 'Search/Results?q=rcl-searchall%3A[' . $query . ']&op=1&qs=1'
+             ),
+             array(
+                'vendor' => 'salem',
+                'dbname' => 'salem',
+                'noquery' =>  'http://0-online.salempress.com'. $proxyStr . 'home.do',
+                'queryUrl' => 'http://0-online.salempress.com'. $proxyStr . 'search.do?categoryName=&All=All&numCategories=5&searchInAll=all&searchText=' . $query
+             ),
+             array(
+                'vendor' => 'naxos',
+                'dbname' => 'naxos',
+                'noquery' =>  'http://0-losrios.naxosmusiclibrary.com'. $proxyStr,
+                'queryUrl' => 'http://0-losrios.naxosmusiclibrary.com'. $proxyStr . 'google/searchgoogle.asp?googletext=' . $query
+             )
+             
+             );
+
+
+if ($ehostCode !== '') {
+    $url = findURL($dbs[0], $query);
+    echo 'url is ' .$url;
 }
 else {
-    $url = $urlBase . ';rgn=full%20text;page=simple';
-}
-
-}
-elseif (strpos($dbName, 'oxford-english') > -1) {
-    $urlBase = 'http://0-www.oed.com.lasiii.losrios.edu/';
-    if ($query !== '') {
-    $url = $urlBase . 'search?searchType=dictionary&q=' . $query;
-    }
-    else {
-        $url = $urlBase;
-    }
-    
-}
-elseif (strpos($dbName, 'oxford-art') > -1) {
-    $urlBase = 'http://0-www.oxfordartonline.com.lasiii.losrios.edu/';
-    if ($query !== '') {
-    $url = $urlBase . 'subscriber/search_results?q=' . $query;
-    }
-    else {
-        $url = $urlBase;
-    }
-    
-}
- elseif (strpos($dbName, 'resources') > -1) {
-    $urlBase = 'http://0-www.rclweb.net.lasiii.losrios.edu/';
-    if ($query !== '') {
-    $url = $urlBase . 'Search/Results?q=rcl-searchall%3A[' . $query . ']&op=1&qs=1';
-    }
-    else {
-        $url = $urlBase;
-    }
-    
-}
-elseif (strpos($dbName, 'films') > -1) {
-    $homeLibrary = $_COOKIE['homeLibrary'];
-    switch ($homeLibrary) {
-        case 'arc':
-        $wID = '240535';
-        break;
-      case 'scc':
-        $wID = '106093';
-        break;
-      case 'crc':
-        $wID = '237206';
-        break;
-      case 'flc':
-        $wID = '237742';
-        break;
-      default:
-       $wID = '107590'; 
-  }
-    $urlBase = 'http://0-digital.films.com.lasiii.losrios.edu/PortalPlaylists.aspx?wid=' .$wID;
-    if ($query !== '') {
-    $url = $urlBase . '&rd=a&q=' . $query;
-//    echo $url;
-//    exit;
-    }
-    else {
-        $url = $urlBase;
-    }
-    
-}
-elseif (strpos($vendor, 'salem') > -1) {
-
-    $urlBase = 'http://0-online.salempress.com.lasiii.losrios.edu/';
-    if ($query !== '') {
-    $url = $urlBase . 'search.do?categoryName=&All=All&numCategories=5&searchInAll=all&searchText=' . $query;
-    }
-    else {
-        $url = $urlBase . 'home.do';
+    for ($i = 1; $i < count($dbs); $i++) { // loop starts with second array element
+        if (strpos($dbName, $dbs[$i]['dbname']) > -1) {
+            $url = findURL($dbs[$i], $query);
+            echo $url;
+            break;
+        }
     }
 }
 
-elseif (strpos($vendor, 'naxos') > -1) {
-    $urlBase = 'http://0-losrios.naxosmusiclibrary.com.lasiii.losrios.edu/';
-    $remainderNax = 'google/searchgoogle.asp?googletext=' . $query;
-    if ($query !== '') {
-    $url = $urlBase .  $remainderNax;
-    }
-    else {
-        $url = $urlBase;
-    }
-    }
-elseif (strpos($vendor, 'kanopy') > -1) {
-    $urlBase = 'https://losrios.kanopystreaming.com/';
-    if ($query !== '') {
-    $url = $urlBase .  's?query=' . $query;
-    }
-    else {
-        $url = $urlBase;
-    }
-    }
 
 
 ?>
