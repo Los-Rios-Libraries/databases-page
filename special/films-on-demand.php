f3e1c87554eeb259bc0cc40f30e30ffa1239778b
jkarlsen <karlsej@scc.losrios.edu>
2018-01-08 13:02:45 -0800

    Change WAM to EZproxy

--- special/films-on-demand.php
+++ special/films-on-demand.php
@@ -1,7 +1,7 @@
     <p>Please select your college:</p>
     <ul style="padding-left:2em; list-style-type:disc;">
-      <li><a href="http://0-fod.infobase.com.lasiii.losrios.edu/PortalPlayLists.aspx?wid=240535" onclick="setCookie('homeLibrary', 'arc', 10);">American River College</a></li>
-      <li><a href="http://0-fod.infobase.com.lasiii.losrios.edu/PortalPlayLists.aspx?wid=237206" onclick="setCookie('homeLibrary', 'crc', 10);">Cosumnes River College</a></li>
-      <li><a href="http://0-fod.infobase.com.lasiii.losrios.edu/PortalPlayLists.aspx?wid=237742" onclick="setCookie('homeLibrary', 'flc', 10);">Folsom Lake College</a></li>
-      <li><a href="http://0-fod.infobase.com.lasiii.losrios.edu/PortalPlayLists.aspx?wid=106093" onclick="setCookie('homeLibrary', 'scc', 10);">Sacramento City College</a></li>
+      <li><a href="https://ezproxy.losrios.edu/login?url=http://fod.infobase.com/PortalPlayLists.aspx?wid=240535" onclick="setCookie('homeLibrary', 'arc', 10);">American River College</a></li>
+      <li><a href="https://ezproxy.losrios.edu/login?url=http://fod.infobase.com/PortalPlayLists.aspx?wid=237206" onclick="setCookie('homeLibrary', 'crc', 10);">Cosumnes River College</a></li>
+      <li><a href="https://ezproxy.losrios.edu/login?url=http://fod.infobase.com/PortalPlayLists.aspx?wid=237742" onclick="setCookie('homeLibrary', 'flc', 10);">Folsom Lake College</a></li>
+      <li><a href="https://ezproxy.losrios.edu/login?url=http://fod.infobase.com/PortalPlayLists.aspx?wid=106093" onclick="setCookie('homeLibrary', 'scc', 10);">Sacramento City College</a></li>
     </ul>
