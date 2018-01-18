2295c53655f40cc7bce031f3fdfbf41602c86cbe
jkarlsen <karlsej@scc.losrios.edu>
2018-01-18 10:03:02 -0800

    Switch WAM to EZproxy in search form

--- search-form.php
+++ search-form.php
@@ -1,4 +1,4 @@
-<form id="multi-search" action="https://0-search-ebscohost-com.lasiii.losrios.edu/login.aspx" method="get" aria-describedby="search-exp">
+<form id="multi-search" action="https://ezproxy.losrios.edu/form?qurl=https%3A%2F%2Fsearch.ebscohost.com" method="post" aria-describedby="search-exp">
  <input type="hidden" name="authtype" value="ip,uid">
  <input type="hidden" name="direct" value="true">
  <input type="hidden" name="profile" value="eds">
