fca334ae4bf012a2f2ca8c0ac02f9b1b56e91ebf
jkarlsen <karlsej@scc.losrios.edu>
2018-12-11 16:45:39 -0800

    Don't load CRC's virtual reference widget if it's being shown in an iframe (for Libwizard compatibility)

--- help/crc.php
+++ help/crc.php
@@ -29,13 +29,13 @@ Phone</h3> <p>(916) 691-7266</p>
 
 // showZ();
 
-
+if ( window.location === window.parent.location ) {
 window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
 d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
 _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
 $.src='//v2.zopim.com/?2ZGZ6mvHkstnvlAyFqfxKyKKP8uvAanZ';z.t=+new Date;$.
 type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-
+}
 
 checkCookies('newWindowLinks');
 
