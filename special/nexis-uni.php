01c292a9f26170a1cc1999702080e51c8b874788
jkarlsen <karlsej@scc.losrios.edu>
2019-04-30 16:24:39 -0700

    intermediary page for Nexis Uni (expiration)

--- special/nexis-uni.php
+++ special/nexis-uni.php
@@ -1,14 +1,30 @@
-<p>Nexis Uni is currently not working correctly when accessed from off-campus.</p>
-<p>Until this problem is resolved, please take the following steps:
+<?php
+$url = $_GET['url'];
+?>
+<style>
+   .special h2 {background:transparent; color:#0f0f0f; font-weight:bold;}
+   .special ol {background:transparent; padding-top:0;}
+   #hide-message {text-align:right;}
+</style>
+<div class="special">
+<p><strong>Access to Nexis Uni will expire on June 30, 2019.</strong></p>
+<p>In the future, you may use alternative strategies to find content normally found in Nexis Uni.
 </p>
+<h2>State and Federal Cases</h2>
+<p>You may find state and federal court decisions using <strong>Google Scholar</strong>.</p>
 <ol>
-    <li>
-   Register for a Nexis Uni account: <a href="https://0-signin-lexisnexis-com.lasiii.losrios.edu/lnaccess/UserRegistration?lookup=1">https://0-signin-lexisnexis-com.lasiii.losrios.edu/lnaccess/UserRegistration?lookup=1</a>
-     </li>
-     <li>Once you&apos;ve registered, go directly to <a class="db-name" href="http://www.nexisuni.com">www.nexisuni.com</a> and sign in to use Nexis Uni. Note that when you are on campus, you might not need to sign in to use the database.</li>
+   <li>Visit Google Scholar.</li>
+   <li>Select the <strong>Case law</strong> radio button underneath the search box.</li>
 </ol>
-<div id="hide-message"><button id="hide-page">In the future, go directly to www.nexisuni.com</button></div>
-
+<h2>Law Reviews</h2>
+<p><strong>OneSearch</strong> contains a large number of legal journal articles from various sources. Try adding the phrase &quot;law review&quot; to your search.</p>
+<h2>Newspapers and other news sources</h2>
+<p>The Library will be adding two databases, <strong>US Major Dailies </strong>and <strong>Access World News</strong>, to replace the news sources in Nexis Uni. In addition, OneSearch features a News source type limiter that can be used to search for news content.</p>
+<h2>Business Profiles</h2>
+<p><strong>Business Source Complete</strong> contains profiles of businesses similar to those found in Nexis Uni.</p>
+<p><a class="db-name" href="<?php echo $url; ?>">Continue to Nexis Uni.</a></p>
+<div id="hide-message"><button id="hide-page">In the future, go directly to Nexis Uni</button></div>
+</div>
 <script>
     
     document.getElementById('hide-page').addEventListener('click', function() {
