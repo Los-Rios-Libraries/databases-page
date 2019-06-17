f22e35a12ffc6e2b67657e213a33d3d239ce8d49
jkarlsen <karlsej@scc.losrios.edu>
2019-06-17 12:40:09 -0700

    Add link to US Major Dailies

--- special/nexis-uni.php
+++ special/nexis-uni.php
@@ -19,7 +19,7 @@ $url = $_GET['url'];
 <h2>Law Reviews</h2>
 <p><strong>OneSearch</strong> contains a large number of legal journal articles from various sources. Try adding the phrase &quot;law review&quot; to your search.</p>
 <h2>Newspapers and other news sources</h2>
-<p>The Library will be adding two databases, <strong>US Major Dailies</strong> and <strong><a href="index.php?az&amp;query=newsbank">Access World News</a></strong>, to replace the news sources in Nexis Uni. In addition, OneSearch features a News source type limiter that can be used to search for news content.</p>
+<p>The Library will be adding two databases, <strong><a href="index.php?az&amp;query=dailies">US Major Dailies</a></strong> and <strong><a href="index.php?az&amp;query=newsbank">Access World News</a></strong>, to replace the news sources in Nexis Uni. In addition, OneSearch features a News source type limiter that can be used to search for news content.</p>
 <h2>Business Profiles</h2>
 <p><strong><a href="index.php?az&amp;query=business+source+complete">Business Source Complete</a></strong> contains profiles of businesses similar to those found in Nexis Uni.</p>
 <p><a class="db-name" href="<?php echo $url; ?>">Continue to Nexis Uni.</a></p>
