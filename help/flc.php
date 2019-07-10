aa9f25f88683de71110340c927c181ad8003646e
Amy Brinkley <amy.brinkley@flc.losrios.edu>
2019-07-10 15:16:49 -0700

    Updated summer hours, removed icons, updated contact information

--- help/flc.php
+++ help/flc.php
@@ -1,44 +1,40 @@
 <div id="flc-help">
-<p>The FLC Librarians can help you with your research in variety of ways.</p>
-<p><em>We are always happy to help!</em></p>
-<div id="services">
-    <div class="service">
-        <a href="http://www.flc.losrios.edu/libraries/ask-a-librarian"><img alt="Online Help Form" src="help/flc-img/chat01.png" height="29" width="29"></a>
-        <h3>Ask A Librarian</h3>
-        <p>    <a href="http://www.flc.losrios.edu/libraries/ask-a-librarian">Online Help Form</a></p>
-    </div>
-    <div class="service">
-        <img alt="Txt" src="help/flc-img/diagram-34.png">
-        <h3>Call Us</h3>
-        <p>916-608-6612</p>
-    </div>
-    <div class="service">
-    <img  alt="Visit Us" src="help/flc-img/user_friend.png" height="32" width="32">
-    <h3>Visit Us</h3>
-    <p>Reference Desk</p>
-    <p>FLC Main Campus Library</p>
-    </div>
-</div>
-<hr>
-<h3>Research Help Hours</h3>
-<table id="library-hours">
-<tr><td>M-Th</td>
-<td>8 a.m. - 8 p.m.</td>
-</tr>
-<tr>
-    <td>Fri</td>
-    <td>8 a.m. - 4:30 p.m.</td>
-</tr>
-</table>
+	<p>Librarians are happy to help you with your research.</p>
+	<h3>Call</h3>
+	<p>916&ndash;608&ndash;6612</p>
+
+	<h3>Email</h3>
+	<p><a href="mailto:flc-librarian@flc.losrios.edu">FLC-Librarian@flc.losrios.edu</a></p>
+	
+	<h3>Visit</h3>
+	<p>FLC Main Campus Library (Aspen Hall)</p>
+	<p>EDC Library (Building A)</p>
+
+	<hr>
+
+	<h3>Library Hours</h3>
+	<p>Summer 2019 | June 10 - August 8<p>
+	<table id="library-hours">
+	<thead>
+		<tr>
+			<th>Day</th>
+			<th>Hours</th>
+		</tr>
+    </thead>
+    <tbody>
+		<tr>
+			<td><abbr title="Monday">Mon</abbr> &ndash; <abbr title="Thursday">Thu</abbr></td>
+			<td>9:00 am to 5:00 pm</td>
+		</tr>
+		<tr>
+			<td><abbr title="Friday">Fri</abbr> - <abbr title="Sunday">Sun</abbr></td>
+			<td>Closed</td>
+		</tr>
+    </tbody>
+	</table>
+
+	<p><a href="https://www.flc.losrios.edu/student-resources/library/about/hours">More about library hours</a></p>
 </div>
 <script>
-/*    (function() { // hide crc chat widget if
-    var crcChat = document.querySelector('.zopim');
-    if (crcChat) {
-        crcChat.className += ' hidden';
-
-    }
-    })(); */
-// hideZ();
-checkCookies('newWindowLinks');
+	checkCookies('newWindowLinks');
 </script>
