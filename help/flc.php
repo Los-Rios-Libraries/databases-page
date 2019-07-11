4d09ec690cae807ca00dfeeb8f5ae35646df117c
Amy Brinkley <amy.brinkley@flc.losrios.edu>
2019-07-11 13:34:30 -0700

    Amend style, adjust hours

--- help/flc.php
+++ help/flc.php
@@ -1,40 +1,30 @@
 <div id="flc-help">
-	<p>Librarians are happy to help you with your research.</p>
+
 	<h3>Call</h3>
 	<p>916&ndash;608&ndash;6612</p>
-
+	<br>
 	<h3>Email</h3>
 	<p><a href="mailto:flc-librarian@flc.losrios.edu">FLC-Librarian@flc.losrios.edu</a></p>
-	
+	<br>
 	<h3>Visit</h3>
-	<p>FLC Main Campus Library (Aspen Hall)</p>
+	<p>FLC Library (Aspen Hall)</p>
 	<p>EDC Library (Building A)</p>
 
 	<hr>
 
 	<h3>Library Hours</h3>
-	<p>Summer 2019 | June 10 - August 8<p>
-	<table id="library-hours">
-	<thead>
-		<tr>
-			<th>Day</th>
-			<th>Hours</th>
-		</tr>
-    </thead>
-    <tbody>
-		<tr>
-			<td><abbr title="Monday">Mon</abbr> &ndash; <abbr title="Thursday">Thu</abbr></td>
-			<td>9:00 am to 5:00 pm</td>
-		</tr>
-		<tr>
-			<td><abbr title="Friday">Fri</abbr> - <abbr title="Sunday">Sun</abbr></td>
-			<td>Closed</td>
-		</tr>
-    </tbody>
-	</table>
-
+	<p>Summer 2019</p>
+	<p>June 10 - August 8</p>
+	<br>
+	<h3>Monday - Thursday</h3>
+	<p>8:00 am to 6:00 pm</p>
+	<br>
+	<h3>Friday, Weekends and Holidays</h3>
+	<p>Closed</p>
+	<br>
 	<p><a href="https://www.flc.losrios.edu/student-resources/library/about/hours">More about library hours</a></p>
 </div>
+
 <script>
 	checkCookies('newWindowLinks');
 </script>
