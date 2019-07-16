d9e0ac678ebb9ebfbc1f51f07ccb0dc7390fa0af
Kate Williamson <w1576672@apps.losrios.edu>
2019-07-16 21:16:13 +0000

    Updated appointments link to LibCal. Removed old appointment widget dialog script.

--- help/arc.php
+++ help/arc.php
@@ -7,42 +7,12 @@
     <p><a href="https://www.arc.losrios.edu/student-resources/library/contact"><img alt="Contact a librarian, phone, email" height="135" src="help/arc-img/AskLibrarian_Menu.png" width="136" /></a></p>
 </div>
     <div id="arc-appt">
-        <p><a href="https://arc_losriosdss.ingeniuxondemand.com/student-resources/library/research/make-an-appointment"><img alt="Research Appointments" height="40" src="help/arc-img/ResearchAppts.png" width="136" /></a></p>
+        <p><a href="https://arc.losrios.libcal.com/appointments"><img alt="Research Appointments" height="40" src="help/arc-img/ResearchAppts.png" width="136" /></a></p>
     </div>
 </div>
 
 <script>
     checkCookies('newWindowLinks');
-
-    function attachDialog() { // from Springshare
-    	$("#mySched27927").LibCalMySched({
-    		iid: 525,
-    		uid: 0,
-    		gid: 1123,
-    		width: 500,
-    		height: 450
-    	});
-
-    }
-    var wait = setInterval(function() { // wait for jQuery to load
-
-    	if (typeof(jQuery) === 'function') {
-    		clearTimeout(wait);
-    		if (!(document.getElementById('arc-sched-scr'))) { // if libcal scheduler script is not already loaded, load it
-    			var a = document.createElement('script');
-    			a.async = true;
-    			a.id = 'arc-sched-scr'; // allows us to check for it before loading
-    			a.src = '//api3.libcal.com/js/myscheduler.min.js?002';
-    			document.getElementsByTagName('body')[0].appendChild(a); // add script below jQuery
-    			$('#arc-sched-scr').on('load', function() { // wait for it to load since we need the LibCalMySched function
-    				attachDialog();
-    			});
-    		} else {
-    			attachDialog();
-    		}
-    	}
-    }, 50);
-    	
   
 </script>
 
