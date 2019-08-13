c4b86c08958f11eb9c7fd726c31bd52a6c233a9a
jkarlsen <karlsej@scc.losrios.edu>
2019-08-13 16:32:35 -0700

    Update SCC hours for Fall 2019

--- help/scc.php
+++ help/scc.php
@@ -17,7 +17,7 @@ style="display: none;">
 </div>
 <hr>
 <table id="library-hours">
-    <caption>Library Hours, Summer 2019 (June 10 - August 8)</caption>
+    <caption>Library Hours, Fall 2019 (August 24 &ndash; December 19)</caption>
     <thead>
     <tr>
         <th>Day</th>
@@ -27,10 +27,18 @@ style="display: none;">
     <tbody>
     <tr>
         <td><abbr title="Monday">M</abbr> &ndash; <abbr title="Thursday">Th</abbr></td>
+        <td>7:30 am &ndash; 9:30 pm</td>
+    </tr>
+    <tr>
+        <td><abbr title="Friday">F</abbr></td>
+        <td>7:30 am &ndash; 5 pm</td>
+    </tr>
+        <tr>
+        <td><abbr title="Saturday">Sa</abbr></td>
         <td>9 am &ndash; 3 pm</td>
     </tr>
     <tr>
-        <td><abbr title="Friday">F</abbr> - <abbr title="Sunday">Su</abbr></td>
+        <td><abbr title="Sunday">Su</abbr></td>
         <td>Closed</td>
     </tr>
     </tbody>
