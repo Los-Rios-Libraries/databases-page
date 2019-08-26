b029438df233d899fc06e94cc90161bd9b602467
jkarlsen <karlsej@scc.losrios.edu>
2019-08-26 12:26:59 -0700

    update crc hours for fall 2019

--- help/crc.php
+++ help/crc.php
@@ -5,19 +5,23 @@
 
 <h3>Hours</h3>
 
-<h4>Spring-Summer Intersession:</h4>
-<p>Closed May 23 - June 11 (Except for paying fines)</p>
-
-<h4>Summer 2019 (June 10 - Aug. 8)</h4>
+<h4>Fall 2019 (Aug. 24 - Dec. 19)</h4>
 <ul>
-<li>Mon - Thurs 8:00 am - 8:00 pm</li>
-<li>Fri - Sun Closed</li>
+<li>Mon - Thurs 7:30 am - 8:00 pm</li>
+<li>Fri 7:30 am - 4:00 pm</li>
+<li>Sat 10:00 am - 4:00 pm</li>
+<li>Sun Closed</li>
 </ul>
-<h4>Holiday:</h4>
+<h4>Holidays:</h4>
 <ul>
-<li>July 4 - Independence Day</li>
-
+<li>Sept. 2 - Labor Day</li>
+<li>Nov. 11 - Veterans' Day</li>
+<li>Nov. 28 - Dec. 1 - Thanksgiving Recess</li>
 </ul>
+
+
+
+
 
 <h3>
 Phone</h3> <p>(916) 691-7266</p>
