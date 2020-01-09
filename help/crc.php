c19372740aede8c769fe2651813c0833d3a8ae0e
jkarlsen <karlsej@scc.losrios.edu>
2020-01-09 11:36:25 -0800

    Remove chat widgets

--- help/crc.php
+++ help/crc.php
@@ -29,22 +29,3 @@ Phone</h3> <p>(916) 691-7266</p>
 
 
 </div>
-
-<!--Start of Zopim Live Chat Script--><script>
- 
-// added: if crc chat as been hidden, will show it -- now not using
-
-// showZ();
-
-if ( window.location === window.parent.location ) {
-window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
-d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-$.src='//v2.zopim.com/?2ZGZ6mvHkstnvlAyFqfxKyKKP8uvAanZ';z.t=+new Date;$.
-type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-}
-
-checkCookies('newWindowLinks');
-
-
-</script><!--End of Zopim Live Chat Script-->
--- help/scc.php
+++ help/scc.php
@@ -1,12 +1,6 @@
-<style>
-  .libchat_btn_img {width:100px !important;}
-</style>
 <div id="scc-help">
-<img src="loader.gif" id="scc-chat-loader"  height="16" alt="loading">
-<div id="libchat_3ed10430124d950ef2b216a68e1b18ba" class="libchat-block"></div>
-<p id="chat-online" style="display: none;" class="libchat-msg">Chat now</p>
-<p id="chat-offline" style="display: none;" class="libchat-msg">Chat offline; leave us a message</p>
-<hr>
+<!-- add chat widget 
+<hr>-->
 <table id="library-hours">
     <caption>Library Hours, Fall 2019 (August 24 &ndash; December 19)</caption>
     <thead>
@@ -36,75 +30,10 @@
 </table>
 <p><a href="//www.scc.losrios.edu/library/about/hours">More about library hours</a></p>
 </div>
-<script src="https://v2.libanswers.com/load_chat.php?hash=3ed10430124d950ef2b216a68e1b18ba"></script>
+
 <script>
   //  hideZ();
 checkCookies('newWindowLinks');
 
-    // libraryh3lp presence
-
-  (function() {
-
-var jqWait = setInterval(function() {
-	if (typeof(jQuery) === 'function')
-	{
-		clearInterval(jqWait);
-    // text that will be placed below icon
-var online = $('#chat-online');
-var offline = $('#chat-offline');
-// alt attributes set in LibChat widget
-var onlineAlt = 'Ask Us';
-var offlineAlt = 'Offline';
-var chatID = '12136'; // widget ID
-var waitForChat = setInterval(function() {
-	console.log('working');
-	var btn = $('.libchat_btn_img'); // button doesn't load until after page load--this is class given it by Springshare
-	if (btn.length) {
-		clearInterval(waitForChat);
-    $('#scc-chat-loader').remove();
-		if (btn.attr('alt') === onlineAlt) { // if it is online, let will need to poll in case it goes offline
-			// move text to inside anchor
-			online.insertAfter(btn).show();
-			// poll API for offline status
-			var checkPresence = setInterval(function() {
-				$.getJSON('https://losrios.libanswers.com/1.0/chat/widgets/status/' + chatID)
-				.done(function(d) {
-					console.log(d);
-					if (d.online !== true) { 
-						clearInterval(checkPresence); // this is just one-way
-						online.hide(2, function() {
-							btn.attr({'style': '40% !important', 'alt': offlineAlt}); // change size of button
-							offline.insertAfter(btn).show(); // move relevant paragraph into anchor
-							// clone to remove event handler
-							var newAnchor = btn.parent().clone();
-							newAnchor.attr('href', '/library/services/ask-librarian');
-							btn.parent().remove();
-							$('.libchat-block').append(newAnchor);
-							
-							
-						});
-					}
-				})
-				.fail(function(a,b,c) {
-					ga('send', 'event', 'libchat presence check', 'error', c);
-				});
-			}, 20000);
-	
-		}
-		
-		else if (btn.attr('alt') === offlineAlt) {
-			clearInterval(waitForChat);
-			offline.insertAfter($('.libchat_btn_img')).show();
-		}
-		
-	}
-	
-}, 1000);
-
-		$('#choose-library button').on('click', function() {
-			clearInterval(check_presence);
-		});
-	}
-}, 100);
-  }());
+  
 </script>
