942e77cad3958d3b85cb4a5d3552503dab13807b
jkarlsen <karlsej@scc.losrios.edu>
2020-07-01 10:13:57 -0700

    Add Wiley Online Library

--- search-db.php
+++ search-db.php
@@ -166,8 +166,8 @@ $dbs = array(
 													
 													),
 														array(
-													'dbname' => 'cochrane',
-													'queryUrl' => 'https://www.cochranelibrary.com/search?p_p_id=scolarissearchresultsportlet_WAR_scolarissearchresults&p_p_lifecycle=0&_scolarissearchresultsportlet_WAR_scolarissearchresults_searchType=basic&_scolarissearchresultsportlet_WAR_scolarissearchresults_searchBy=6&_scolarissearchresultsportlet_WAR_scolarissearchresults_searchText=' . $query					
+													'dbname' => 'wiley',
+													'queryUrl' => $ezpStr . 'https://www.onlinelibrary.wiley.com/action/doSearch?AllField=' . $query		. '&PubType=journal'			
 													)
              
              );
