13ae1b59195ace2803a7c42d9c256a305eb87cc9
Andi Adkins Pogue <w0583482@apps.losrios.edu>
2020-07-12 20:18:53 +0000

    update crc database hours to reflect virtual support

--- help/crc.php
+++ help/crc.php
@@ -3,29 +3,15 @@
         Location</h3>
     <p>Library Building (2nd Floor)</p>
 
-<h3>Hours</h3>
+<h3>Important Announcement</h3>
 
-<h4>Fall 2019 (Aug. 24 - Dec. 19)</h4>
-<ul>
-<li>Mon - Thurs 7:30 am - 8:00 pm</li>
-<li>Fri 7:30 am - 4:00 pm</li>
-<li>Sat 10:00 am - 4:00 pm</li>
-<li>Sun Closed</li>
-</ul>
-<h4>Holidays:</h4>
-<ul>
-<li>Sept. 2 - Labor Day</li>
-<li>Nov. 11 - Veterans' Day</li>
-<li>Nov. 28 - Dec. 1 - Thanksgiving Recess</li>
-</ul>
-
-
-
+<p>The physical library is closed.<br>
+<a href="https://researchguides.crc.losrios.edu/library_closure">More about virtual support</a></p>
 
 
 <h3>
 Phone</h3> <p>(916) 691-7266</p>
-<h3>Email</h3> <p><a href="mailto:library@crc.losrios.edu">library@crc.losrios.edu</a></p>
+<h3>Email</h3> <p><a href="mailto:library@losrios.libanswers.com">library@losrios.libanswers.com</a></p>
 
 
 </div>
