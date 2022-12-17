9d762e238afefbd10339c7e9dfbcbf38fdf1d3f3
jkarlsen <karlsej@scc.losrios.edu>
2022-12-17 11:46:58 -0800

    Fix typo

--- banner.php
+++ banner.php
@@ -2,6 +2,6 @@ showNote({
 	cid: 'db_survey',
 	message: 'Do you want to help us improve this page?  <a href="https://forms.gle/pSi5XmGdg7ZV78PGA" target="_blank">Tell us why you&apos;re here.</a>',
 	//start: '2018-05-18',
-	end: '2023-12-15',
+	end: '2022-12-15',
 	exp: 30
 });
